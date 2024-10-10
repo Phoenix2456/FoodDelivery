e3c9a9e71e74a71e4990ace8c7e64c81b9dce231
Benny <phoenixshadow2456@gmail.com>
2024-10-10 01:54:35 +0000

    Ask the user's name, order, address, and the order's address

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello CodeSandbox!")
+user_name = input("What's your name? ")
+order = input("Place Your order: ")
+order_address = input("Your order address: ")
+address = input("Please place your address: ")
